ae0a0d10b19d403a362ae7ef14ca8ee272fbcdc6
rakkarage <rakkarage@gmail.com>
2022-06-08 19:55:05 -0400

    x

--- src/TileMap.js
+++ src/TileMap.js
@@ -5,17 +5,6 @@ class TileMap {
 		this.tileSize = 16
 		this.mapWidth = 8
 		this.mapHeight = 9
-		this.map = [
-			13, 13, 13, 13, 13, 13, 13, 13,
-			13, 13, 13, 13, 13, 13, 13, 13,
-			13, 13, 13, 13, 13, 13, 13, 13,
-			13, 13, 13, 13, 13, 12, 13, 13,
-			13, 13, 13, 13, 13, 13, 13, 13,
-			13, 13, 13, 20, 13, 13, 13, 13,
-			13, 13, 13, 13, 20, 13, 13, 13,
-			13, 13, 13, 13, 12, 13, 13, 13,
-			13, 13, 13, 13, 13, 13, 13, 13
-		]
 		this.ssCliff = loadSpriteSheet("img/Tile/Cliff.png", 16, 16, 20)
 		this.ssDoodad = loadSpriteSheet("img/Tile/Doodad.png", 16, 16, 72)
 		this.ssEdge = loadSpriteSheet("img/Tile/Edge.png", 16, 16, 123)
@@ -25,37 +14,30 @@ class TileMap {
 		this.ssRubble = loadSpriteSheet("img/Tile/Rubble.png", 16, 16, 6)
 		this.ssTheme = loadSpriteSheet("img/Tile/Theme.png", 16, 16, 116)
 		this.ssWater = loadSpriteSheet("img/Tile/Water.png", 16, 16, 56)
-		this.mapBack = new TileMapLayer(this.ssTheme, this.mapWidth, this.mapHeight)
-		this.mapFore = new TileMapLayer(this.ssTheme, this.mapWidth, this.mapHeight)
-		// onready var _back: TileMap = $Level / Viewport / Back
-		// onready var _fore: TileMap = $Level / Viewport / Fore
-		// onready var _flower: TileMap = $Level / Viewport / Flower
-		// onready var _waterBack: TileMap = $Level / Viewport / WaterBack
-		// onready var _splitBack: TileMap = $Level / Viewport / SplitBack
-		// onready var _itemBack: TileMap = $Level / Viewport / ItemBack
-		// onready var _tree: TileMap = $Level / Viewport / Tree
-		// onready var _itemFore: TileMap = $Level / Viewport / ItemFore
-		// onready var _splitFore: TileMap = $Level / Viewport / SplitFore
-		// onready var _waterFore: TileMap = $Level / Viewport / WaterFore
-		// onready var _top: TileMap = $Level / Viewport / Top
-		// onready var _light: TileMap = $Level / Viewport / Light
-		// onready var _edge: TileMap = $Level / Viewport / Edge
+		this.mapLayers = [
+			Layer(this.ssTheme),
+			Layer(this.ssTheme),
+			Layer(this.ssOutside),
+			Layer(this.ssWater),
+			Layer(this.ssDoodad),
+			Layer(this.ssWater),
+			Layer(this.ssDoodad),
+			Layer(this.ssLight),
+			Layer(this.ssEdge)
+		]
 	}
-	GetMap(x, y) {
-		return this.map[this.GetIndex(x, y, this.mapWidth)]
-	}
-	GetIndex(x, y, width) {
-		return y * width + x
-	}
-	GetPosition(index) {
-		var y = Math.floor(index / this.mapWidth)
-		var x = Math.floor(index - this.mapWidth * y)
-		return [x, y]
+	Layer(spriteSheet) {
+		return new TileMapLayer(spriteSheet, this.tileSize, this.mapWidth, this.mapHeight)
 	}
 	Draw() {
 		for (var y = 0; y < this.mapHeight; y++) {
 			for (var x = 0; x < this.mapWidth; x++) {
-				this.ssTheme.drawFrame(this.GetMap(x, y), x * this.tileSize, y * this.tileSize)
+				this.mapLayers.forEach(layer => {
+					var tile = layer.GetMap(x, y)
+					if (tile != -1) {
+						layer.ss.drawFrame(tile, x * this.tileSize, y * this.tileSize)
+					}
+				});
 			}
 		}
 	}
--- src/TileMapLayer.js
+++ src/TileMapLayer.js
@@ -6,4 +6,15 @@ class TileMapLayer {
 		this.mapHeight = h
 		this.data = Array(w * h).fill(-1)
 	}
+	GetMap(x, y) {
+		return this.data[this.GetIndex(x, y, this.mapWidth)]
+	}
+	GetIndex(x, y, width) {
+		return y * width + x
+	}
+	GetPosition(index) {
+		var y = Math.floor(index / this.mapWidth)
+		var x = Math.floor(index - this.mapWidth * y)
+		return [x, y]
+	}
 }
